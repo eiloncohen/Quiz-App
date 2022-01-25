e66aabda6f69b96b2abc1727d23fcb871df52ada
eiloncohen <eiloncohen20@gmail.com>
2022-01-25 18:46:28 +0200

    add feedback to player, keep Score and fix the bugs (final commit)

--- quiz_brain.py
+++ quiz_brain.py
@@ -8,14 +8,12 @@ class QuizBrain:
         self.score = 0
         self.question_list = q_list
         self.current_question = None
-        # self.ui = None
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
-        # self.ui = UI(self.current_question)
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {q_text}"
@@ -24,6 +22,10 @@ class QuizBrain:
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
+            print("answer is correct!")
             return True
         else:
+            print("wrong!")
             return False
+
+
--- ui.py
+++ ui.py
@@ -11,22 +11,51 @@ class UI(Tk):
         self.quiz = quiz_brain
         self.configure(padx=20, pady=20, bg=THEME_COLOR)
         self.title("Quizzer")
-        self.score_num = 0
         self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
         self.question_text = self.canvas.create_text(150, 125, text="Some Question Text", fill=THEME_COLOR,
                                                      font=("Ariel", 20, "italic"), width=280)
         self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=50)
-        self.score = Label(text=f"Score: {self.score_num}", fg="white", font=("Ariel", 20, "italic"),
+        self.score = Label(text=f"Score: {self.quiz.score}", fg="white", font=("Ariel", 20, "italic"),
                            bg=THEME_COLOR, highlightthickness=0)
         self.score.grid(row=0, column=1, pady=20)
         self.correct_img = PhotoImage(file=CORRECT_IMG_PATH)
-        self.known_button = Button(image=self.correct_img, highlightthickness=0, command=self.enter_question_to_canvas)
+        self.known_button = Button(image=self.correct_img, highlightthickness=0,
+                                   command=self.true_pressed)
         self.known_button.grid(row=2, column=0)
         self.wrong_img = PhotoImage(file=WRONG_IMG_PATH)
-        self.known_button = Button(image=self.wrong_img, highlightthickness=0, command=self.enter_question_to_canvas)
-        self.known_button.grid(row=2, column=1)
+        self.unknown_button = Button(image=self.wrong_img, highlightthickness=0,
+                                   command=self.false_pressed)
+        self.unknown_button.grid(row=2, column=1)
+        self.enter_question_to_canvas()
         self.mainloop()
 
     def enter_question_to_canvas(self):
-        next_question = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=next_question, fill=THEME_COLOR)
+        self.canvas.configure(bg='white')
+        if self.quiz.still_has_questions():
+            next_question = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=next_question, fill=THEME_COLOR)
+        else:
+            self.canvas.itemconfig(self.question_text, text="Game Over", fill=THEME_COLOR)
+            self.known_button.config(state="disabled")
+            self.unknown_button.config(state="disabled")
+
+    def true_pressed(self):
+        self.is_answer_correct("True")
+
+    def false_pressed(self):
+        self.is_answer_correct("False")
+
+    def is_answer_correct(self, answer):
+        is_correct = self.quiz.check_answer(answer)
+        if is_correct:
+            self.canvas.configure(bg='green')
+            self.score.configure(text=f"Score: {self.quiz.score}")
+        else:
+            self.canvas.configure(bg='red')
+        self.after(1000, self.enter_question_to_canvas)
+
+
+
+
+
+
